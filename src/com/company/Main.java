a7cf4f4a65ad5bff1bc71df7b66a2b326c30c325
Radiker <sasharadionov@bk.ru>
2022-02-07 21:07:20 +0800

    Add comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,22 @@ import java.util.Arrays;
 public class Main {
     public static double[][] matrixMultiply(double[][] matrix1, double[][] matrix2) {
         // WRITE CODE HERE
-        int length = Math.max(matrix1[0].length, matrix1.length);
-        int height = Math.min(matrix1[0].length, matrix1.length);
-        double[][] matrix = new double[length][length];
-        for(int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
+        //Результатом умножения матриц A(m * n) и B(n * k) будет матрица C(m * k)
+        //Вычисление разрядности матриц
+        int m = matrix1.length;
+        int n = matrix1[0].length;
+        int k = matrix2[0].length;
+        //объявляем результативную матрицу
+        double[][] matrix = new double[m][k];
+        //Проходимся по элементам матрицы
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < k; j++) {
+                //Обнуляем значение
                 matrix[i][j] = 0;
-                for (int l = 0; l < height; l++) {
-                    if(matrix1[0].length > matrix1.length)
-                        matrix[i][j] += matrix1[l][i] * matrix2[j][l];
-                    else
-                        matrix[i][j] += matrix1[i][l] * matrix2[l][j];
+                //Суммируем произведения элементов i-строки 1 матрицы
+                //на соответствующие элементы j-того столбца 2 матрицы
+                for (int l = 0; l < n; l++) {
+                    matrix[i][j] += matrix1[l][i] * matrix2[j][l];
                 }
             }
         }
@@ -25,6 +30,7 @@ public class Main {
     public static void main(String[] args) {
         double[][] matrix1 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
         double[][] matrix2 = {{3, 2, 1}, {3, 2, 1}, {3, 2, 1}};
+
         System.out.println(Arrays.deepToString(matrixMultiply(matrix1, matrix2)));
     }
 }
